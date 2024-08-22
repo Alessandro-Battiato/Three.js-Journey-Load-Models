c833aec3c832445a0a0ab1a951bdedc564f8c555
alessandrobattiato <alessandroDev@outlook.it>
2024-08-22 12:59:57 +0200

    feat: add animation

--- src/Fox.js
+++ src/Fox.js
@@ -1,8 +1,20 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import React from "react";
 
 const Fox = () => {
     const fox = useGLTF("./Fox/glTF/Fox.gltf");
+    const animations = useAnimations(fox.animations, fox.scene);
+
+    useEffect(() => {
+        // Sometimes the actions object might be empty on initial renders, thus we use the animations inside a use effect
+        animations.actions.Run.play();
+
+        window.setTimeout(() => {
+            animations.actions.Walk.play();
+            animations.actions.Walk.crossFadeFrom(animations.actions.Run, 1);
+        }, 2000);
+    }, []);
+
     return (
         <primitive
             object={fox.scene}
