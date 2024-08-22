eb213579032c28777c27490ff4609427145fe8c8
alessandrobattiato <alessandroDev@outlook.it>
2024-08-22 12:05:52 +0200

    refactor: use useGltf to load models

--- src/Model.js
+++ src/Model.js
@@ -1,25 +1,32 @@
 import React from "react";
+import { useGLTF } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import { DRACOLoader } from "three/examples/jsm/Addons.js";
 import { useLoader } from "@react-three/fiber";
 
 const Model = () => {
-    const model = useLoader(
-        GLTFLoader,
-        // files in the public folder are available at the first level like this
-        "./FlightHelmet/glTF/FlightHelmet.gltf", // in order to test the lazy loading model, we need to use the helmet instead of the burger because the burger loads way too fast, thanks also to the draco loader
-        (loader) => {
-            const dracoLoader = new DRACOLoader();
-            dracoLoader.setDecoderPath("./draco/"); // only provide the folder
-            loader.setDRACOLoader(dracoLoader);
-        }
-    );
+    {
+        /*
+        const model = useLoader(
+            GLTFLoader,
+            // files in the public folder are available at the first level like this
+            "./hamburger.glb", // in order to test the lazy loading model, we need to use the helmet instead of the burger because the burger loads way too fast, thanks also to the draco loader
+            (loader) => {
+                const dracoLoader = new DRACOLoader();
+                dracoLoader.setDecoderPath("./draco/"); // only provide the folder
+                loader.setDRACOLoader(dracoLoader);
+            }
+        );
+    */
+    }
+
+    const model = useGLTF("./hamburger.glb"); // this is the easiest and most efficient way to load models, the previous one was the "behind the scenes"
 
     {
         /*To load models, you need to use the primitive tag*/
     }
 
-    return <primitive object={model.scene} scale={5} position-y={-1} />;
+    return <primitive object={model.scene} scale={0.35} />;
 };
 
 export default Model;
